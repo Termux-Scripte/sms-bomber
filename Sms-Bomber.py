2448520525b4d7bdebb7ab3a949e7a8ca203b49a
Termux-Scripte <139042906+Termux-Scripte@users.noreply.github.com>
2023-08-28 16:37:55 +0330

    Update Sms-Bomber.py

--- Sms-Bomber.py
+++ Sms-Bomber.py
@@ -822,7 +822,7 @@ r='\033[1;31m'
 g='\033[32;1m' 
 y='\033[1;33m'
 w='\033[1;37m'
-printLow(f'{r}▒█▀▀▀█ ░▀░ █▀▀█ ▒█▀▀█ █░░█ ░▀░ █▀▀ █░░█\n{w}░▀▀▀▄▄ ▀█▀ █▄▄▀ ▒█▄▄█ █▀▀█ ▀█▀ ▀▀█ █▀▀█\n{g}▒█▄▄▄█ ▀▀▀ ▀░▀▀ ▒█░░░ ▀░░▀ ▀▀▀ ▀▀▀ ▀░░▀\n\n{y}Info:\n    {g}[+] {y}TelegramPv: {w}@i1amali\n    {g}[+] {y}TelegramChannel: {w}@@termux_scripte\n   \n{y}system:\n    {g}[+] {y}Platform: {w}{System}\n    {g}[+] {y}Node: {w}{Node}\n    {g}[+] {y}Release: {w}{Release}\n\n')
+printLow(f'{r}▒█▀▀▀█ ░▀░ █▀▀█ ▒█▀▀█ █░░█ ░▀░ █▀▀ █░░█\n{w}░▀▀▀▄▄ ▀█▀ █▄▄▀ ▒█▄▄█ █▀▀█ ▀█▀ ▀▀█ █▀▀█\n{g}▒█▄▄▄█ ▀▀▀ ▀░▀▀ ▒█░░░ ▀░░▀ ▀▀▀ ▀▀▀ ▀░░▀\n\n{y}Info:\n    {g}[+] {y}TelegramPv: {w}@King_Nexus \n    {g}[+] {y}TelegramChannel: {w}@termux_scripte\n   \n{y}system:\n    {g}[+] {y}Platform: {w}{System}\n    {g}[+] {y}Node: {w}{Node}\n    {g}[+] {y}Release: {w}{Release}\n\n')
 def Vip(phone, Time):
     Thread(target=snap, args=[phone]).start(), sleep(Time)
     Thread(target=gap, args=[phone]).start(), sleep(Time)
@@ -876,5 +876,3 @@ while True:
     try: Vip(phone, Time)
     except KeyboardInterrupt: exit(f'{r}[-] User Exited')
     except: print(f'{r}[-] Error TimeOut')
-# Channel: SirPhish
-# Coder; @imNotValid - :)
